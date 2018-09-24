05cef5cd5dd90cf14bdf29f738146a18e8b10843
DeekshaGowda <deekshacs03@gmail.com>
2018-09-23 21:55:57 -0400

    features and labels

--- dataLoader.py
+++ dataLoader.py
@@ -1,31 +1,56 @@
 import os
 import json
+import pickle
+import numpy as np
 from PIL import Image
+from collections import defaultdict
 
 
-def load_data():
+def load_data(img_size):
     """
-
-    :return:
+    Create pickle objects of features and labels 
+    :param img_size: the new size of the re-sized image
+    :return: None
     """
     images_dir = "./resources/log"
     labels_dir = "./resources/json_data"
-    throttle, angle = [],  []
+    features, y_angle, y_throttle = [], [], []
 
-    for path, dir, files in os.walk(labels_dir):
+    for path, dir, files in os.walk(images_dir):
         for file in files:
-            with open(os.path.join(path, file)) as f:
-                data = json.load(f)
-                angle.append(data['user/angle'])
-                throttle.append(data['user/throttle'])
 
-    print(len(throttle), len(angle))
+            if file.endswith('.jpg'):
+                img_id = file.split('_')[0]
+                json_record = "record_" + img_id + ".json"
+
+                # resize and convert to grey scale
+                img = Image.open(os.path.join(path, file))
+                img = img.resize(img_size).convert('L')
+                features.append(list(img.getdata()))
+
+                # get throttle and angle
+                with open(os.path.join(labels_dir, json_record)) as f:
+                    data = json.load(f)
+                    y_angle.append(data['user/angle'])
+                    y_throttle.append(data['user/throttle'])
+
+
+    print("%d features, %d angles, %d throttle" % (len(features), len(y_angle), len(y_throttle)))
+
+    X = np.array(features).astype('float32')
+    y_angle = np.array(y_angle).astype('float32')
+
+    with open("features", "wb") as f:
+        pickle.dump(X, f)
+
+    with open("angles", "wb") as f:
+        pickle.dump(y_angle, f)
 
 
 if __name__ == '__main__':
-    load_data()
-    img = Image.open("./resources/log/0_cam-image_array_.jpg")
-    pix_val = list(img.getdata())
-    pix_flat = [channel for pixel in pix_val for channel in pixel]
-    print(len(pix_val))
-    print(img.format, img.size, img.mode)
+
+    images = load_data((80, 60))
+
+    # img = Image.open("./resources/log/0_cam-image_array_.jpg")
+    # print("Input image", img.format, img.size, img.mode)
+    # img.show()
